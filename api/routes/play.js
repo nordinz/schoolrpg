1e2f9413edfdb919b55d60b95bcf28d8b0763d97
nordinz <nordin.zerhouni@gmail.com>
2023-05-17 12:09:37 +0200

    cleanup!

--- api/routes/play.js
+++ api/routes/play.js
@@ -2,13 +2,6 @@ const express = require('express');
 const { playGame } = require('../services');
 const router = express.Router();
 
-router.get('/toss', async (req, res) => {
-  try {
-    playGame(req, res);
-  } catch (error) {
-    console.log(error.message);
-    return res.status(500).json('Server error!');
-  }
-});
+router.get('/toss', playGame);
 
 module.exports = router;
--- api/services.js
+++ api/services.js
@@ -4,7 +4,7 @@ function rollDice(diceQuantity) {
     const dice = Math.floor(Math.random() * 6) + 1;
     diceArray.push(dice);
   }
-  console.log(diceArray);
+  // console.log(diceArray);
   const totalValue = diceArray.reduce(
     (accumulator, currentValue) => accumulator + currentValue,
     0
@@ -14,22 +14,27 @@ function rollDice(diceQuantity) {
 }
 
 function playGame(req, res) {
-  const diceQuantity = req.query.quantity;
-  if (isNaN(diceQuantity) || diceQuantity <= 0) {
-    return res.status(400).json('Please enter a valid number above 0');
-  }
+  try {
+    const diceQuantity = req.query.quantity;
+    if (isNaN(diceQuantity) || diceQuantity <= 0) {
+      return res.status(400).json('Please enter a valid number above 0');
+    }
 
-  let characterDice = rollDice(diceQuantity);
-  let monsterDice = rollDice(diceQuantity);
-  let result;
-  if (monsterDice > characterDice) {
-    result = 'Monster wins!';
-  } else if (monsterDice < characterDice) {
-    result = 'Character wins!';
-  } else {
-    result = 'Its a tie, you live to fight another day!';
+    let characterDice = rollDice(diceQuantity);
+    let monsterDice = rollDice(diceQuantity);
+    let result;
+    if (monsterDice > characterDice) {
+      result = 'Monster wins!';
+    } else if (monsterDice < characterDice) {
+      result = 'Character wins!';
+    } else {
+      result = 'Its a tie, you live to fight another day!';
+    }
+    return res.json({ result, monsterDice, characterDice, diceQuantity });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json('Server error!');
   }
-  return res.json({ result, monsterDice, characterDice, diceQuantity });
 }
 module.exports = {
   playGame,
